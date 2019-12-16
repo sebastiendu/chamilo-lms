aa34d00612c07c5e863437e1056eacb00dccb020
Sébastien Ducoulombier <seb@ldd.fr>
2019-12-16 13:07:05 +0100

    phpCAS externalisation - refs BT#16484

--- main/lang/french/trad4all.inc.php
+++ main/lang/french/trad4all.inc.php
@@ -6813,6 +6813,7 @@ $CasMainPortComment = "Le port sur lequel se connecter au serveur CAS principal"
 $CasMainProtocolTitle = "Protocole du serveur CAS principal";
 $CAS1Text = "CAS 1";
 $CAS2Text = "CAS 2";
+$CAS3Text = "CAS 3";
 $SAMLText = "SAML";
 $CasMainProtocolComment = "Le protocole par lequel on se connecte au serveur CAS";
 $CasUserAddActivateTitle = "Activer la création d'utilisateurs via CAS";
